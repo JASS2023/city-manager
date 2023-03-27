603f1110f75fcb730f319116e86f90ae1079f5c1
Philipp Zagar <philipp.zagar@gmail.com>
2023-03-27 09:47:07 +0300

    Init

---
+++ JASS 2023 Smart City/JASS 2023 Smart City/JASS_2023_Smart_CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  JASS_2023_Smart_CityApp.swift
+//  JASS 2023 Smart City
+//
+//  Created by Philipp Zagar on 27.03.23.
+//
+
+import SwiftUI
+
+@main
+struct JASS_2023_Smart_CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
